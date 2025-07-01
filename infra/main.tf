9f7bbcebbfaf3e8b6e37db248b0da92f30b9146b
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-01 21:44:22 +0100

    modified main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,12 +2,48 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "web3-demo-cluster"
-  cluster_version = "1.27"
-  subnets         = ["subnet-xxxxxx", "subnet-yyyyyy"]
-  vpc_id          = "vpc-xxxxxx"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  manage_aws_auth = true
+  name = "election-dapp-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
+
+  cluster_name    = "election-dapp-cluster"
+  cluster_version = "1.31"
+
+  # Optional
+  cluster_endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  vpc_id     = "vpc-1234556abcdef"
+  subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
