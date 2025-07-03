17d0343f970cf131ed5125fb33e9f30bd1ad2cda
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-03 22:19:40 +0100

    added s3 creation file

---
+++ infra/s3.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "election-DApp-terraform-state"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "Terraform State Storage"
+    Environment = "dev"
+  }
+}
