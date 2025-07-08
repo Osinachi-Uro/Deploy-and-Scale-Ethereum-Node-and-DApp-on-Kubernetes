e0dfb98137f1dc24c0597a02696b8ce137564e8c
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-08 13:04:22 +0100

    create s3 bucket on AWS

---
+++ s3/s3.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "election-DApp-terraform-state-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "Terraform State Storage"
+    Environment = "dev"
+  }
+}
