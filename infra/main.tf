3dedd38059a2b979677f6b58e27d6fe5255dd82d
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-08 22:34:03 +0100

    fix error

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0.0"
+    }
+  }
+
   backend "s3" {
     bucket = "election-dapp-terraform-state-bucket"
     key    = "eks/terraform.tfstate"
