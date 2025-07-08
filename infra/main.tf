2cbe309f878fb1c15514c8a72a1379ee07786808
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-08 16:42:57 +0100

    modified bucket name

--- infra/main.tf
+++ infra/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "election-dapp-terraform-state"
+    bucket = "election-dapp-terraform-state-bucket"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
