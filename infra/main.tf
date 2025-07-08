a0855ac7e0cc550ada02288514e718cf76fecdc6
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-08 23:35:53 +0100

    modified infra workflow and main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,12 @@
 terraform {
-  # required_providers {
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = ">= 6.0.0"
-  #   }
-  # }
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.34"
+    }
+  }
 
   backend "s3" {
     bucket = "election-dapp-terraform-state-bucket"
