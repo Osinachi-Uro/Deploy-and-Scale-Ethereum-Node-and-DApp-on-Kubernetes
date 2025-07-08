497a265c0543a3528dbb7780eee17c69d671e3b0
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-08 21:55:00 +0100

    fix version

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.3.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.2.0"
+      version = ">= 5.95, < 6.0.0"
     }
   }
 
