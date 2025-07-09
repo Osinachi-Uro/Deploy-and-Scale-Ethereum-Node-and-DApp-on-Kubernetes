340c823e85cff75df920dfd2ed1a20b41237a362
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-09 18:13:32 +0100

    downgrade version

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 0.12"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0.0"
+      version = ">= 3.68.0"
     }
   }
 
@@ -14,48 +16,41 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "var.region"
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
 
-  name = "election-dapp-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.30.0"
+  version = "20.8.4"
 
   cluster_name    = "election-dapp-cluster"
-  cluster_version = "1.33"
+  cluster_version = ">=2.7.1"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
+  eks_managed_node_group_defaults = {
+    instance_types = ["t2.micro"]
+  }
+
   eks_managed_node_groups = {
     general = {
       desired_capacity = 1
-      max_capacity     = 1
+      max_capacity     = 2
       min_capacity     = 1
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t2.micro"]
+      ami_type       = "AL2023_x86_64_STANDARD"
       subnet_ids     = module.vpc.private_subnets
     }
   }
+  # cluster_compute_config = {
+  #   enabled    = true
+  #   node_pools = ["general-purpose"]
+
+  # }
 
   # Referencing VPC outputs from the module
   vpc_id     = module.vpc.vpc_id
