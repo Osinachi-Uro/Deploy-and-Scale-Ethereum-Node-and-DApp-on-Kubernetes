005a78e999765f9a7a19d0ddbb8587f5faaf6d51
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-07-14 18:56:56 +0100

    removed AmazonEKSAdminPolicy

--- infra/iam.tf
+++ infra/iam.tf
@@ -22,8 +22,7 @@ locals {
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ]
 }
 
