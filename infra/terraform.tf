3272ac2b7758359e08ce340b65b87040aee44dd1
Osinachi-Uro <osinachi.ibiam@gmail.com>
2025-08-06 14:44:35 +0100

    modified provider config

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -5,6 +5,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.3.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10"
+    }
   }
 
   required_version = "~> 1.3"
